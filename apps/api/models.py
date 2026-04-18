a85433fd6bd20c8e0d51af24847d9c396a1bba28
Daylen Smith <daylencsmith@gmail.com>
2026-04-17 20:45:07 -0700

    add tag and bridge entities

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,7 +1,7 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from uuid import UUID, uuid4
 from datetime import datetime
-from typing import Optional, Any
+from typing import Optional, Any, List
 
 class Project(SQLModel, table=True):
     __tablename__ : Any = "projects"
@@ -18,3 +18,20 @@ class Project(SQLModel, table=True):
     live_url: Optional[str] = None
     is_featured: bool = False
     created_at: datetime = Field(default_factory=datetime.utcnow)
+
+class ProjectTagLink(SQLModel, table=True):
+    __tablename__: Any = "project_tag_links"
+    
+    project_id: UUID = Field(foreign_key="projects.id", primary_key=True)
+    tag_id: UUID = Field(foreign_key="tags.id", primary_key=True)
+
+class Tag(SQLModel, table=True):
+    __tablename__: Any = "tags"
+    
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(unique=True, index=True) # e.g., "C++"
+    slug: str = Field(unique=True, index=True) # e.g., "cpp"
+    
+    # Relationship back to projects
+    projects: List["Project"] = Relationship(back_populates="tags", link_model=ProjectTagLink)
+    
